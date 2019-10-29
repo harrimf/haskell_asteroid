44d28c727ae9b47d626d58459a8e531d96bac2cd
Harrison Foko <h.t.moudefoko@students.uu.nl>
2019-10-29 14:03:48 +0100

    dataTypes

---
+++ .stack-work/dist/e626a42b/build/asteroid/autogen/Paths_asteroid.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_asteroid (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "C:\\haskell_development\\game\\asteroid\\.stack-work\\install\\2015b563\\bin"
+libdir     = "C:\\haskell_development\\game\\asteroid\\.stack-work\\install\\2015b563\\lib\\x86_64-windows-ghc-8.6.5\\asteroid-0.1.0.0-DpzFwg4qUsH131WHpzTCzs-asteroid"
+dynlibdir  = "C:\\haskell_development\\game\\asteroid\\.stack-work\\install\\2015b563\\lib\\x86_64-windows-ghc-8.6.5"
+datadir    = "C:\\haskell_development\\game\\asteroid\\.stack-work\\install\\2015b563\\share\\x86_64-windows-ghc-8.6.5\\asteroid-0.1.0.0"
+libexecdir = "C:\\haskell_development\\game\\asteroid\\.stack-work\\install\\2015b563\\libexec\\x86_64-windows-ghc-8.6.5\\asteroid-0.1.0.0"
+sysconfdir = "C:\\haskell_development\\game\\asteroid\\.stack-work\\install\\2015b563\\etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "asteroid_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "asteroid_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "asteroid_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "asteroid_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "asteroid_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "asteroid_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
--- src/Model.hs
+++ src/Model.hs
@@ -2,6 +2,8 @@
 --   which represent the state of the game
 module Model where
 
+import Graphics.Gloss
+
 data InfoToShow = ShowNothing
                 | ShowANumber Int
                 | ShowAChar   Char
@@ -9,10 +11,73 @@ data InfoToShow = ShowNothing
 nO_SECS_BETWEEN_CYCLES :: Float
 nO_SECS_BETWEEN_CYCLES = 5
 
-data GameState = GameState {
-                   infoToShow  :: InfoToShow
-                 , elapsedTime :: Float
-                 }
+-- data GameState = GameState {
+--                    infoToShow  :: InfoToShow
+--                  , elapsedTime :: Float
+--                  }
 
-initialState :: GameState
-initialState = GameState ShowNothing 0
+-- initialState :: GameState
+-- initialState = GameState ShowNothing 0
+
+type Angle = Float
+type FireDelay = Float -- time between each shot
+type Level = Int 
+type Position = Point
+type Score = Int
+type Size = Vector
+type Velocity = Vector
+
+data AccelerationState = Accelerating | Decelerating | Still
+data ShootState = Shooting | NotShooting
+data RotateState = RotatingRight | RotatingLeft | NotRotating
+
+
+data GameState = GameState {  
+  state :: State,
+  level :: Level,
+  world :: World
+}
+
+data State = Playing | Paused | GameOver
+
+data World = World {  
+  score         :: Int,
+  size          :: Size,
+  asteroids     :: [Asteroid],
+  bombs         :: [Bomb],
+  players       :: [Player],
+  hostileShips  :: [HostileShip],
+  projectiles   :: [Projectile]
+}
+
+data SpaceShip = SpaceShip {  
+  position  :: Position,
+  velocity  :: Velocity,
+  size      :: Size
+}
+
+data Projectile = Projectile {  
+  position  :: Position,
+  velocity  :: Velocity,
+  size      :: Size
+} 
+
+data Asteroid = Asteroid {  
+  position  :: Position,
+  velocity  :: Velocity,
+  size      :: Size
+}
+
+data Bomb = Bomb {
+  position  :: Position,
+  velocity  :: Velocity,
+  size      :: Size
+ }
+
+data HostileShip = HostileShip {
+  position  :: Position,
+  velocity  :: Velocity,
+  size      :: Size,
+  angle     :: Angle,
+  fireRate  :: FireDelay
+}
