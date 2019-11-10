5d8d91a857a352f4b2383397bfbf7ed13fc322f3
Floris Roos <floris_roos@hotmail.com>
2019-11-10 21:52:37 +0100

    even more changes

--- src/View.hs
+++ src/View.hs
@@ -12,10 +12,10 @@ viewPure :: World -> Picture
 viewPure world@(World {..}) = pictures ([render player, render projectiles, render asteroids] ++ pausePicture ++ lastTime ++ gameOverPicture)
     where
         pausePicture
-            | state == Paused = [ translate ((-1)*200) 0 $ scale 0.5 0.5 $ color green (text "Paused")]
+            | state == Paused = [ translate ((-1)*130) 0 $ scale 0.5 0.5 $ color green (text "Paused")]
             | otherwise = []
         gameOverPicture
-            | state == GameOver = [ translate ((-1)*150) 0 $ scale 0.4 0.4 $ color red (text "Game Over")]
+            | state == GameOver = [ translate ((-1)*130) 0 $ scale 0.4 0.4 $ color red (text "Game Over")]
             | otherwise = []
         lastTime
             | state == Paused = [ translate ((-1)*150) 100 $ scale 0.1 0.1 $ color yellow $ text lastGameTime]
